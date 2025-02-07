dc82009510d881d01387ccc90e0cd128a56724bf
dil2743 <dil141097@gmail.com>
2025-02-07 21:20:03 +0000

    pcd module completed

--- workspace/ldd/driver/002psudo_char_driver/pcd.c
+++ workspace/ldd/driver/002psudo_char_driver/pcd.c
@@ -3,6 +3,8 @@
 #include<linux/cdev.h>
 #include<linux/device.h>
 #include<linux/kdev_t.h>
+#include<linux/uaccess.h>
+
 #define DEV_MEM_SIZE 512
 #undef pr_fmt
 #define pr_fmt(fmt) "%s:" fmt,__func__
@@ -13,18 +15,81 @@ dev_t device_number;
 struct cdev pcd_cdev;
 
 
-loff_t pcd_lseek(struct file *filep, loff_t off , int whence){
+loff_t pcd_lseek(struct file *filp, loff_t offset , int whence){
 	pr_info("lseek requested \n");
-	return 0;
+	loff_t tmp;
+
+	
+	pr_info("current file pointer is at %lld\n",filp->f_pos);
+	switch(whence){
+	
+		case SEEK_SET:
+			if((offset > DEV_MEM_SIZE) || (offset < 0))
+				return -EINVAL;
+			filp->f_pos = offset;
+			break;
+		case SEEK_CUR:
+			tmp = (filp->f_pos + offset);
+			if( tmp > DEV_MEM_SIZE ||  tmp < 0 )
+				return -EINVAL;
+			filp->f_pos += offset;
+			break;
+		case SEEK_END:
+			tmp = DEV_MEM_SIZE + offset;
+			if(tmp > DEV_MEM_SIZE || tmp <0 )
+				return -EINVAL;
+			filp->f_pos = DEV_MEM_SIZE + offset; 
+			break;
+		default:
+			return -EINVAL;
+
+	}
+	pr_info("Now value of the file position is %lld\n",filp->f_pos);
+	return filp->f_pos;
 }
-ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *pos){
+ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_pos){
 	pr_info("read requested %zu bytes\n",count);
-	return 0;
+	pr_info("Current file position %lld\n", *f_pos);
+	/*1. check user requested count value as the device has limited storage */
+	if((*f_pos + count) > DEV_MEM_SIZE)
+		count = DEV_MEM_SIZE - *f_pos;
+	/*2. copy count number of bytes from device memory to user buffer*/
+	if((copy_to_user(buff, &device_buffer[*f_pos], count)))
+		return -EFAULT;
+	/*3. update the f_pos position pointer */
+	*f_pos += count;
+	
+	pr_info("Number of bytes succeffult read = %zu, \n", count);
+	pr_info("File postion udated = %lld\n",*f_pos);
+	/*4. return number of bytes succeffuly read or error code */
+	/*5. if f_ops at EOF, then rerurn 0 */
+	return count;
 }
 
-ssize_t pcd_write(struct file *flip, const char __user *buff, size_t count, loff_t *pos){
+ssize_t pcd_write(struct file *flip, const char __user *buff, size_t count, loff_t *f_pos){
+	
 	pr_info("write requested %zu bytes\n",count);
-	return 0;
+        pr_info("Current file position %lld\n", *f_pos);
+
+        /*1. check user requested count value as the device has limited storage */
+        if((*f_pos + count) > DEV_MEM_SIZE)
+                count = DEV_MEM_SIZE - *f_pos;
+
+	if(!count){
+		pr_error("No Memory Left on device!!!\n");
+		return -ENOMEM;
+	}
+        /*2. copy count number of bytes from device memory to user buffer*/
+        if(copy_from_user(&device_buffer[*f_pos], buff, count))
+                return -EFAULT;
+        /*3. update the f_pos position pointer */
+        *f_pos += count;
+
+        pr_info("Number of bytes succeffult written = %zu, \n", count);
+        pr_info("File postion udated = %lld\n",*f_pos);
+        /*4. return number of bytes successfully written or error code */
+        /*5. if f_ops at EOF, then rerurn 0 */
+        return count;
 }
 
 int pcd_open(struct inode *inode, struct file *flip){
@@ -41,6 +106,7 @@ struct file_operations pcd_fops = {
 	.write = pcd_write,
 	.read = pcd_read,
 	.llseek = pcd_lseek,
+	.release = pcd_release,
 	.owner = THIS_MODULE
 };
 
